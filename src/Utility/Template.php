53a14b487a0ca3a3e3033bce58150a909edf00d3
Laurent Desmonceaux <ldesmonceaux@meilleurtaux.com>
2020-01-21 11:47:07 +0100

    template

---
+++ src/Utility/Template.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laurent.d
+ * Date: 1/21/20
+ * Time: 11:35 AM
+ */
+
+namespace QuinenLib\Utility;
+
+use Cake\Utility\Hash;
+
+class Template
+{
+
+    public function __invoke($template, $data)
+    {
+        return self::templateArray($template, $data);
+    }
+
+    public static function templateArray($template, $data)
+    {
+        if (is_scalar($template)) {
+            return self::templateString($template, $data);
+        } else {
+            if (is_array($template)) {
+                return collection($template)->map(function ($oneTemplate) use ($data) {
+                    return self::templateArray($oneTemplate, $data);
+                })->toArray();
+            } else {
+                return $template;
+            }
+        }
+    }
+
+    public static function templateString($string, $data)
+    {
+        preg_match_all('#\{\{([\w\._]+)\}\}#', $string, $matches);
+        $string = collection($matches)->transpose()->reduce(function ($reducer, $match) use ($data) {
+            $value = Hash::get($data, $match[1]);
+            $reducer = str_replace($match[0], $value, $reducer);
+            return $reducer;
+        }, $string);
+        return $string;
+    }
+}
--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,11 @@
 <?php
+
+use QuinenLib\Utility\Template;
+
+if (!function_exists('template')) {
+    function template($template, $data)
+    {
+        $templater = new Template();
+        return $templater($template, $data);
+    }
+}
