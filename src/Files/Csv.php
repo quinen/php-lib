79d8cfed8ca217261d639f0d52984ef684b85f61
Laurent Desmonceaux <ldesmonceaux@meilleurtaux.com>
2020-02-03 13:53:50 +0100

    export CSV

--- src/Files/Csv.php
+++ src/Files/Csv.php
@@ -4,8 +4,13 @@
 namespace QuinenLib\Files;
 
 
+use Cake\ORM\Entity;
+use QuinenLib\Arrays\MapTrait;
+
 class Csv
 {
+    use MapTrait;
+
     public static function fileToArray($fileString, $options = [])
     {
         $options += [
@@ -25,5 +30,54 @@ class Csv
                 )->toArray();
             }
         )->toArray();
+    }
+
+    public static function fromData($data, $options = [])
+    {
+        $options += [
+            'delimiter' => ',',
+            'enclosure' => '""',
+            'escapeChar' => "\\",
+            'isHeader' => true
+        ];
+        $f = fopen('php://memory', 'rb+');
+        collection($data)->each(function ($item, $index) use ($f, $options) {
+
+            $fputcsv = function ($data) use ($f, $options) {
+                return fputcsv($f, $data, $options['delimiter'], $options['enclosure'], $options['escapeChar']);
+            };
+
+            /** @var Entity $item * */
+            if ($item instanceof Entity) {
+                if ($index === 0 && $options['isHeader']) {
+                    $fields = $item->getVisible();
+                    return $fputcsv($fields);
+
+                }
+                $item = $item->toArray();
+            }
+
+            return $fputcsv($item);
+        });
+        rewind($f);
+        return stream_get_contents($f);
+    }
+
+    /*
+     *
+     * */
+    public static function fromMap($data, $maps, $options = [])
+    {
+        //TODO a implementer
+    }
+
+    public function getMapFormat($map)
+    {
+        return $map;
+    }
+
+    public function getMapLabel($field)
+    {
+        return $field;
     }
 }
