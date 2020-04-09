07b4df25e296081de4e600aa0d3be977f749d986
Laurent Desmonceaux <github@quinen.net>
2020-04-09 18:50:49 +0200

    dates format

---
+++ src/Utility/Dates.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laurent
+ * Date: 09/04/2020
+ * Time: 16:07
+ */
+
+namespace QuinenLib\Utility;
+
+
+class Dates
+{
+    //http://userguide.icu-project.org/formatparse/datetime
+    const FORMAT_JOURNOM_JOUR_MOIS_ANNEE_HEURE_MINUTE = 'ccc d MMM yyyy H:mm';
+    const FORMAT_MOIS_ANNEE = 'MMMM yyyy';
+}
