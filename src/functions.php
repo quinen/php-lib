0b952d206e5059e7639feb2228d59f7ac53ed863
Laurent Desmonceaux <ldesmonceaux@meilleurtaux.com>
2020-06-29 18:20:22 +0200

    test hook

--- src/functions.php
+++ src/functions.php
@@ -30,6 +30,7 @@ if (!function_exists('debug_lite')) {
         $info = $dbbt[0]['file'] . '.' . $dbbt[0]['line'];
 
         if (PHP_SAPI === 'cli') {
+
             echo PHP_EOL . str_pad($info, 80, '_', STR_PAD_LEFT) . PHP_EOL . var_export($var, true) . PHP_EOL;
         } else {
             echo '<pre>' . PHP_EOL . $info . PHP_EOL . var_export($var, true) . PHP_EOL . '</pre>';
